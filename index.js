7afb11b2390dd36dfdf91183c35a11c0353f8458
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-04-22 19:48:36 +0300

    Update

---
+++ index.js
@@ -0,0 +1,42 @@
+// TASK 1
+// My resolving
+// let number1 = prompt("Ввести значення number1");
+// let number2 = prompt("Ввести значення number2");
+
+
+// let sum = parseInt(number1) + parseInt(number2);
+// let sum1 = Math.pow(number1, 2) + Math.pow(number2, 2);
+// let sum2 = Math.pow(sum, 2);
+
+// console.log("number1:", number1);
+// console.log("number2:", number2);
+// console.log("Сумма введених значень:", sum);
+// console.log("Сумма квадратів введених значень:", sum1);
+// console.log("Квадрат суми введених значень:", sum2);
+
+// Ciklum resolving
+// function exercise1(number1, number2) {
+//     const sum = number1 + number2;
+//     const sum1 = Math.pow(number1, 2) + Math.pow(number2, 2);
+//     const sum2 = Math.pow(sum, 2);
+//     console.log(sum);
+//     console.log(sum1);
+// }
+
+// exercise1(5, 4);
+
+
+
+// TASK 2
+// My resolving
+// let a = prompt("Ввести першу сторону трикутника");
+// let b = prompt("Ввести другу сторону трикутника");
+// let c = prompt("Ввести третю сторону трикутника");
+// let perimeter = parseInt(a) + parseInt(b) + parseInt(c);
+// alert(`Периметр трикутника дорівнює ${perimeter}`);
+
+// Ciklum resolving
+// function exercise2(a, b, c) {
+// const perimeter = a + b + c;
+// console.log(perimeter);
+// }
