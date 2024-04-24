fe27fa94ecc0c3407612d9a6971a54c3736c4c56
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-04-24 12:12:45 +0300

    Update

--- index.js
+++ index.js
@@ -72,3 +72,62 @@
 
 
 
+
+const array = [
+    "7", "4", "12", "8", "3", "5", "7", "6", "6", "14",
+    "11", "13", "9", "1", "8", "11", "0", "14", "2", "10",
+    "2", "2", "2", "13", "5", "11", "14", "8", "7", "8",
+    "3", "2", "13", "7", "1", "14", "1", "13", "10", "9",
+    "2", "5", "5", "8", "5", "4", "4", "2", "11", "0",
+    "13", "4", "10", "11", "11", "4", "11", "8", "2", "10",
+    "9", "14", "11", "7", "8", "10", "11", "3", "10", "6",
+    "8", "4", "4", "13", "1", "0", "8", "3", "12", "8",
+    "4", "7", "12", "12", "0", "1", "14", "9", "10", "3",
+    "9", "12", "10", "2", "9", "5", "1", "13", "8", "5"
+];
+const result = {};
+
+
+// Метод reduce()
+var result1 = array.reduce(function(acc, elem) {
+  acc[elem] = (acc[elem] || 0) + 1;
+  return acc;
+}, {});
+
+console.log("Метод reduce:", result1); // --> {'0': 4, '1': 6, ...}
+
+// Цикл for
+const result2 = {};
+for (var i = 0; i < array.length; ++i)
+{
+    var a = array[i];
+    if (result2[a] != undefined)
+        ++result2[a];
+    else
+        result2[a] = 1;
+}
+
+console.log("Цикл for:", result2);
+
+// Метод forEach()
+const result3 = {};
+array.forEach(function(a){
+    if (result3[a] != undefined)
+        ++result3[a];
+    else
+        result3[a] = 1;
+});
+
+console.log("Метод forEach:", result3);
+
+// Цикл for of
+const result4 = {};
+for (let elem of array) {
+    if (result4[elem] === undefined) {
+    result4[elem] = 1;
+    } else {
+    result4[elem]++;
+    }
+}
+
+console.log("Цикл for of:", result4);
